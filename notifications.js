1ee10d8cf326cf5e47e034ac102fc9e829bf3d56
thoufiq135 <shaikno150@gmail.com>
2026-06-18 00:23:15 +0530

    added all data in response

--- notifications.js
+++ notifications.js
@@ -28,22 +28,22 @@ async function fetchUnreadAndMarkRead(userId) {
 router.get("/getNotifications",rateLimiter,async(req,res)=>{
   try {
     console.log("came to notifications")
-    //  const header = req.headers.authorization;
-    // if (!header) {
-    //   return res.status(401).json({ message: "Missing authorization headers" });
-    // }
+     const header = req.headers.authorization;
+    if (!header) {
+      return res.status(401).json({ message: "Missing authorization headers" });
+    }
     
-    // const token = header.split(" ")[1];
-    // if (!token) {
-    //   return res.status(401).json({ message: "Token not found" });
-    // }
+    const token = header.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Token not found" });
+    }
 
-    // const payload = await veriftJWT(token);
-    // if (!payload) {
-    //   return res.status(401).json({ message: "Invalid or expired token" });
-    // }
+    const payload = await veriftJWT(token);
+    if (!payload) {
+      return res.status(401).json({ message: "Invalid or expired token" });
+    }
 
-    const userId = "6a26fbb4f3108bbd35659f95"; 
+    const userId = payload.id; 
     
 console.log("email=",userId)
     const notifications = await fetchUnreadAndMarkRead(userId);
